1fd44b24fea3c55e1208ae57da67f94eda793c8e
InoDash <81249652+inocent345f@users.noreply.github.com>
2025-01-23 18:16:33 +0100

    Update model.py

--- model.py
+++ model.py
@@ -1,16 +1,32 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class Signup(BaseModel):
     username: str
     email: str
-    password : str
+    password: str
+    profile_picture: Optional[str] = None
     
 class Login(BaseModel):
     username: str
     password: str
     
-
 class VerifyOtp(BaseModel):
     type: str
     token: str
     email: str
+
+class UpdateProfilePicture(BaseModel):
+    username: str
+    file_data: str  # Base64 encoded image data
+
+class UpdateUserProfile(BaseModel):
+    username: str
+    name: Optional[str] = None
+    phone: Optional[str] = None
+    description: Optional[str] = None
+    profile_picture_url: Optional[str] = None
+
+class RemoveProfilePicture(BaseModel):
+    username: str
+
